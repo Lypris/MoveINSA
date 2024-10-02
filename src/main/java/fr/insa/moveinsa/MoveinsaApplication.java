6bc366c8f93db52adbe5a426e579104a9e346521
Lypris <rudy.virquin@gmail.com>
2024-10-02 15:36:21 +0200

    first commit

---
+++ src/main/java/fr/insa/moveinsa/MoveinsaApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.moveinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveinsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoveinsaApplication.class, args);
+    }
+
+}
